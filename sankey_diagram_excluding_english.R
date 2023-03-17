470003076421098b7845e8713309f453b6ab1d06
Abhishek Bhardwaj <abhishek02bhardwaj.er@gmail.com>
2023-03-18 02:21:21 +0530

    Sankey Diagrams

---
+++ sankey_diagram_excluding_english.R
@@ -0,0 +1,13 @@
+library(circlize)
+
+# Read in the CSV file
+mydata <- read.csv("C:/Users/abhis/PycharmProjects/outreachy/task 5/output.csv")
+
+# Manipulate the data to prepare it for the chord diagram
+mydata <- mydata[mydata$translations_from != "en" & mydata$translations_to != "en", c("translations_from", "translations_to", "article_count")] # select the columns you need
+colnames(mydata) <- c("From", "To", "Value") # rename the columns
+mydata <- aggregate(Value ~ From + To, mydata, sum) # group by From and To, summing the values
+
+# Create the chord diagram
+chordDiagram(mydata, directional = 1, direction.type = c("diffHeight", "arrows"), link.arr.type = "big.arrow")
+
